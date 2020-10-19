fe86a2efeec6dc5eacc35a091c5443c24867427e
Apoorva Singh <apoorvasingh157@gmail.com>
2020-10-19 23:03:37 +0530

    Rule class parses instruction like clockwork :D

--- rule.py
+++ rule.py
@@ -1,69 +1,53 @@
+from typing import Dict
 from typing import List
-import json
+
 import instructions
+
 
 class RuleParsingException(Exception):
     pass
 
+
 class InvalidInstructionException(RuleParsingException):
     pass
 
+
 class Rule:
-    '''Rule is a collection of Conditions(Instructions) chained together.'''
+    """Rule is a collection of Conditions(Instructions) chained together."""
 
     # Look up table for instruction decoding
-    instruction_lut: List = [
+    instruction_lut: List = {
         "at_time": instructions.AtTime,
-        "at_time_for_x_occurence": instructions.AtTimeWithOccurence,
-        "is_relay_state": instructions.IsRelayState,
-        "is_relay_state_for": instructions.IsRelayStateFor,
-        "check_temperature": instructions.CheckTemperature,
-        "check_temperature_for": instructions.CheckTemperatureFor,
+        "at_time_with_occurrence": instructions.AtTimeWithOccurrence,
+        "relay_state": instructions.IsRelayState,
+        "relay_state_for": instructions.IsRelayStateFor,
+        "temperature": instructions.CheckTemperature,
+        "temperature_for": instructions.CheckTemperatureFor,
         "door_window_state": instructions.DoorWindowState,
         "door_window_state_for": instructions.DoorWindowStateFor,
         "check_occupancy_for": instructions.CheckOccupancyFor,
         "check_occupancy": instructions.CheckOccupancy,
         "logical_and": instructions.LogicalAnd,
         "logical_or": instructions.LogicalOr,
-    ]
+        "energy_meter": instructions.EnergyMeter,
+    }
 
-
-    def __init__(self, rule_name, json_data: str):
-        '''Converts JSON data into rule object'''
+    def __init__(self, rule_dict: Dict):
+        """Converts JSON data into rule object"""
 
         self.instruction_stream: List = []
-        self.name = rule_name
 
-        # Parse JSON data
-        try:
-            rule = json.loads(json_data)
+        # Each rule has a bunch of instructions, Parse the instructions
+        # Add add them to instruction_stream instance variable
+        # Parse individual instructions
+        for ins_data in rule_dict:
+            if ins_data["operation"] in Rule.instruction_lut:
+                Instruction = Rule.instruction_lut[ins_data["operation"]]
+                self.instruction_stream.append(Instruction)
 
-            # Each rule has a bunch of instructions, Parse the instructions
-            # Add add them to instruction_stream instance variable
-            ### Parse individual instructions ###
-            for ins_data in rule:
-                if ins_data["operation"] in Rule.instruction_lut:
-                    Instruction = Rule.instruction_lut[ins_data["operation"]]
-                    self.instruction_stream.append(Instruction)
-                
-                else:
-                    raise InvalidInstructionException(f"Unknown instruction: {ins_data['operation']}")
+            else:
+                raise InvalidInstructionException(f"Unknown instruction: {ins_data['operation']}")
 
-        except json.JSONDecodeError:
-            print("Unable to decode JSON")
-            raise RuleParsingException("Unable to decode JSON")
-        
-        return self
-    
     def infix_to_postfix(self):
         # TODO Perform infix to postfix conversion to make it easier for the VM to evaluate the rule
         pass
-
-
-
-if __name__ == "__main__":
-    json_data = {
-        "operation": "at_time",
-        "time": ""
-    }
-    pass
