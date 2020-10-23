1c511aef0d0158224b26d381e4c74898368819b0
Apoorva Singh <apoorvasingh157@gmail.com>
2020-10-23 16:58:08 +0530

    Added pytest-trio for writing tests trio async tests.
    We'll ignore all the warnings being generated by the test system for the time being.

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,7 +1,14 @@
 import store
+import trio
 
 
-def test_get_device_document_sync():
-    data = store.get_device_document_sync("test-podnet-switch-1")
-    assert data["type"] == "switch"
+class TestStore:
+    def test_get_device_document_sync(self):
+        data = store.get_device_document_sync("test-podnet-switch-1")
+        assert data["type"] == "switch"
 
+    async def test_sleep(self):
+        start_time = trio.current_time()
+        await trio.sleep(1)
+        end_time = trio.current_time()
+        assert end_time - start_time >= 1
