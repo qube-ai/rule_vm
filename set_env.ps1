ee4045560175ef81a4ad67080aefe106cb20bfed
Apoorva Singh <apoorvasingh157@gmail.com>
2020-10-24 12:56:51 +0530

    Shell script for setting env variables and virtual env on Windows and Linux.

---
+++ set_env.ps1
@@ -0,0 +1,3 @@
+$env:GOOGLE_APPLICATION_CREDENTIALS=".\firebase_creds.json"
+$env:SENDGRID_API_KEY="SG.6uTQkIavTR6R-i5G04aa8w.d1kuqALGaZHUEVvTuk-TdHDEfPTHv-MLwgG-rkCWauo"
+.\venv\Scripts\activate
---
+++ set_env.sh
@@ -0,0 +1,3 @@
+export GOOGLE_APPLICATION_CREDENTIALS=".\firebase_creds.json"
+export SENDGRID_API_KEY="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"
+source venv/bin/activate
