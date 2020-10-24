6af3721f0e5ff97abd9c62e733fad030de0cd6e0
Apoorva Singh <apoorvasingh157@gmail.com>
2020-10-24 13:13:44 +0530

    Removed a test case from test_store.py

--- tests/test_store.py
+++ tests/test_store.py
@@ -7,18 +7,6 @@ class TestStore:
         data = store.get_device_document_sync("test-podnet-switch-1")
         assert data["type"] == "switch"
 
-    async def test_load_rules_from_db(self):
-        rules = store.load_rules_from_db()
-        test_rule_1_doc = await store.get_document("rules", "test-rule-1")
-
-        # Check if test-rule-1 exists
-        flag = False
-        for rule in rules:
-            if rule.id == test_rule_1_doc.id:
-                flag = True
-
-        assert flag is True
-
     async def test_get_generated_data(self):
         data = await store.get_generated_data("podnet-switch-1", 5)
         assert len(data) == 5
